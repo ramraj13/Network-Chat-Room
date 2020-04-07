3955dbd4a387b0e968b5cd2da63cf1ab6160fc4b
Rishiraj Dey <48823057+ramraj13@users.noreply.github.com>
2020-04-07 23:24:28 +0530

    Add files via upload

---
+++ client1.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+import time
+
+
+host=socket.gethostbyname(socket.gethostname())
+port=5000
+
+client=socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
+client.connect((host,port))
+
+
+message=input("-->")
+while message !='q':
+
+        while True:
+
+                client.send(message.encode('utf-8'))
+                data=client.recv(1024).decode()
+                if not str(data):
+                        break
+                print("[Message INTERCEPTED::]Received data--> "+data)
+                message=input("-->")
+
+client.close()
---
+++ server1.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+import time
+
+host=socket.gethostbyname(socket.gethostname())
+port=5000
+address=(host,port)
+server=socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
+server.bind(address)
+
+def client(conn,address1,connections):
+    while True:
+        data=conn.recv(1024)
+        if not str(data.decode()):
+            print(f"Client {address1}is Disconnected... ")
+            break
+
+        print(f"Data received from {address1} is {str(data.decode())}")
+
+        for con in connections:
+            if con!= conn:
+                con.send(data)
+
+
+
+    conn.close()
+
+
+def handle_clients(conn,clients):
+    pass
+
+def start():
+    clients=[]
+    connections=[]
+    server.listen()
+    print("[Starting Server]Connecting to server...")
+    while True:
+        conn,address=server.accept()
+        print(f"server connected at{address}")
+        if address not in clients:
+            clients.append(address)
+
+        if conn not in connections:
+            connections.append(conn)
+
+
+        r1=threading.Thread(target=client,args=(conn,address,connections))
+        r1.start()
+
+
+
+
+if __name__=='__main__':
+    start()
