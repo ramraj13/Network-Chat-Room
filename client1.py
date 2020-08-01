ce889deedf87ef4411ae28bde396298f54d7e9ec
Rishiraj Dey <dramraj85@gmail.com>
2020-08-01 09:27:21 +0530

    updated-client-communication
    The spontaneous bug fixed for rapid and smooth chat messaging.

--- client1.py
+++ client1.py
@@ -2,24 +2,29 @@ import socket
 import threading
 import time
 
-
 host=socket.gethostbyname(socket.gethostname())
 port=4444
+print(host)
 
 client=socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
 client.connect((host,port))
 
+def receive():
+        data=client.recv(1024).decode()
+        print("[Message intercepted]"+data)
+        print('-->')
+        receive()
 
-message=input("-->")
-while message !='q':
+def sent():
+        message=input('-->')
+        client.send(message.encode('utf-8'))
+        sent()
 
-        while True:
+t_receive=threading.Thread(target=receive)
 
-                client.send(message.encode('utf-8'))
-                data=client.recv(1024).decode()
-                if not str(data):
-                        break
-                print("[Message INTERCEPTED::]Received data--> "+data)
-                message=input("-->")
+if __name__=="__main__":
+        t_receive.start()
+        sent()
 
-client.close()
+        client.close()
+
